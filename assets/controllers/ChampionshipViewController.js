9dec82181991a82b0c419d8adebe2ae5da7f0d0e
Basic <basic@localhost>
2015-09-23 04:50:13 -0400

    laying out and data mining for ticket purchase projections

--- assets/controllers/ChampionshipViewController.js
+++ assets/controllers/ChampionshipViewController.js
@@ -29,11 +29,32 @@
 				var getPoolsPromise = poolMgmt.getPools(championshipData.id);
 				getPoolsPromise.then(function(poolData) {
 
+					poolData.forEach(function(pool) {
+						pool.poolTotal = 0;
+						pool.ticketCost = 1500;
+						if(pool.entities) {
+							pool.entities.forEach(function(entity) {
+								entity.entityTotal = 0;
+								entity.entityReservations = 0;
+								if(entity.customers) {
+									entity.customers.forEach(function(customer) {
+										if(customer.reservations) {
+											customer.reservations.forEach(function(reservation) {
+												if(reservation.total) {
+													entity.entityTotal += reservation.total;
+													entity.entityReservations += reservation.quantity;
+													pool.poolTotal += reservation.total;
+												}
+											});
+										}
+									});
+								}
+							});
+						}
+					});
+
 					championshipData.pools = poolData;
 
-					console.log('championshipData:');
-					console.log(championshipData);
-		
 					$scope.championship = championshipData;
 				});
 
